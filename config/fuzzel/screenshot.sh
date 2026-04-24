7db7c49411391f78567c58866f971bd4c3c6281a
pharaa <evgenijvavilonov201@gmail.com>
2026-04-24 16:36:54 +0500

    Added small cooldown to hide the menu before screenshot

--- config/fuzzel/screenshot.sh
+++ config/fuzzel/screenshot.sh
@@ -8,12 +8,15 @@ choice=$(echo -e "$options" | fuzzel --dmenu --lines 3)
 
 case $choice in
     "Region")
+        sleep 0.5
         hyprshot -m region -z -o ~/Pictures/Screenshots/
         ;;
     "Screen")
+        sleep 0.5
         hyprshot -z -m output -o ~/Pictures/Screenshots/
         ;;
     "Window")
+        sleep 0.5
         hyprshot -z -m window -o ~/Pictures/Screenshots/
         ;;
 esac
